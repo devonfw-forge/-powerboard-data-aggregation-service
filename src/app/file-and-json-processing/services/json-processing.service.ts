3376ce47456eb51d33bac7d00e2749332ba35059
Hussain <azhar.a.hussain@capgemini.com>
2022-01-13 18:57:51 +0530

    map Json Object into a generic object done

--- src/app/file-and-json-processing/services/json-processing.service.ts
+++ src/app/file-and-json-processing/services/json-processing.service.ts
@@ -1,22 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { Group } from '../models/group';
+import { Property } from '../models/property';
 import { IJsonProcessingService } from './json-processing.service.interface';
+
 
 @Injectable()
 export class JsonProcessingService implements IJsonProcessingService {
-  constructor() {}
+    constructor() { }
 
-  processJson(obj: any): any {
-    return this.flattenJSON(obj, {});
-  }
-
-  flattenJSON(obj: any, res: any, extraKey = ''): any {
-    for (let key in obj) {
-      if (typeof obj[key] !== 'object') {
-        res[extraKey + key] = obj[key];
-      } else {
-        this.flattenJSON(obj[key], res, `${extraKey}${key}_`);
-      }
+    flattenJSON(obj: any, res: any, extraKey = ''): any {
+        for (let key in obj) {
+            if (typeof obj[key] !== 'object') {
+                res[extraKey + key] = obj[key];
+            } else {
+                this.flattenJSON(obj[key], res, `${extraKey}${key}_`);
+            }
+        }
+        return res;
     }
-    return res;
-  }
+
+    processJson(obj: any): any {
+
+        const JSON_Obj = this.flattenJSON(obj, {});
+        let group = {} as Group;
+        group.properties = [];
+        for (var k in JSON_Obj) {
+
+            let properties: Property = {
+                key: k,
+                value: JSON_Obj[k]
+            }
+            group.properties.push(properties);
+        }
+        return group;
+    }
+
 }
