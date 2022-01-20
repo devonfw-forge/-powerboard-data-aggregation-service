83705f60973cbc5df29cf0453843f08d4091b5bb
krishnaCG-1997 <krishna.a.agarwal@capgemini.com>
2022-01-20 18:49:52 +0530

    sprint snapshot entity created with sprint obj in ingestion service

--- src/app/data-ingestion/services/data-ingestion.service.ts
+++ src/app/data-ingestion/services/data-ingestion.service.ts
@@ -6,6 +6,7 @@ import { Group } from '../../file-and-json-processing/models/group';
 // import { InjectRepository } from '@nestjs/typeorm';
 // import { Repository } from 'typeorm/repository/Repository';
 import { Sprint } from '../model/entities/sprint.entity';
+import { SprintSnapshot } from '../model/entities/sprintSnapshot.entity';
 import { SprintStatus } from '../model/entities/sprint_status.entity';
 import { SprintWorkUnit } from '../model/entities/sprint_work_unit.entity';
 import { Team } from '../model/entities/team.entity';
@@ -24,15 +25,14 @@ export class DataIngestionService extends TypeOrmCrudService<Sprint> implements 
     super(sprintRepository);
   }
 
-
   async ingest(processedJson: Group[], teamId: string): Promise<any> {
     let sprintArray: Sprint[] = [];
     for (let group of processedJson) {
-      let sprint: Sprint = {} as Sprint
+      let sprint: Sprint = {} as Sprint;
       //let index = 0;
       for (let object of group.properties) {
         let key = object.key;
-        let splittedKeys = key.split("_");
+        let splittedKeys = key.split('_');
         var actualKey = splittedKeys[splittedKeys.length - 1];
         // index = Number(splittedKeys[splittedKeys.length - 2]);
         // if (sprintArray.length === index - 1) {
@@ -40,75 +40,83 @@ export class DataIngestionService extends TypeOrmCrudService<Sprint> implements 
         //   sprintArray[index - 1] = sprint;
         //   sprint = {} as Sprint
         // }
-        if (actualKey === "id") {
-          console.log(object.value)
+        if (actualKey === 'id') {
+          console.log(object.value);
           sprint.sprint_number = Number(object.value);
         }
-        if (actualKey === "startDate") {
-          console.log(object.value)
+        if (actualKey === 'startDate') {
+          console.log(object.value);
           sprint.start_date = object.value;
         }
-        if (actualKey === "state") {
-          console.log(object.value)
+        if (actualKey === 'state') {
+          console.log(object.value);
 
-          if (object.value === "active") {
-            object.value = 'Completed'
-          }
-          else {
-            object.value = 'In Progress'
+          if (object.value === 'active') {
+            object.value = 'Completed';
+          } else {
+            object.value = 'In Progress';
           }
           const sprintStatus = await this.sprintStatusRepository.findOne({ where: { status: object.value } });
           sprint.status = sprintStatus!.id;
         }
-        if (actualKey === "endDate") {
-          console.log(object.value)
+        if (actualKey === 'endDate') {
+          console.log(object.value);
           sprint.end_date = object.value;
         }
-        if (actualKey === "workUnit") {
-          console.log(object.value)
+        if (actualKey === 'workUnit') {
+          console.log(object.value);
           const sprintWorkUnit = await this.sprintWorkUnitRepository.findOne({ where: { work_unit: object.value } });
           sprint.work_unit = sprintWorkUnit!.id;
         }
-
       }
       const team = await this.teamRepository.findOne({ where: { id: teamId } });
-      sprint.team = team!
+      sprint.team = team!;
       sprintArray.push(sprint);
       // console.log(sprint);
     }
     await this.sprintRepository.findOne('20155bf8-ada5-495c-8019-8d7ab76d488e');
     console.log(sprintArray);
+    const sprintSnapshotArray = this.createSprintSnapshotEntity(sprintArray);
+    console.log(sprintSnapshotArray);
     return sprintArray;
   }
+
+  createSprintSnapshotEntity(sprintArray: Sprint[]): SprintSnapshot[] {
+    let sprintSnapshotArray: SprintSnapshot[] = [];
+    for (let sprint of sprintArray) {
+      let sprintSnapshot: SprintSnapshot = {} as SprintSnapshot;
+      sprintSnapshot.sprint = sprint;
+      sprintSnapshot.date_time = '';
+      sprintSnapshotArray.push(sprintSnapshot);
+    }
+    return sprintSnapshotArray;
+  }
 }
-   // console.log(sprint)
+// console.log(sprint)
 
-    // sprint.sprintNumber=
-    // sprint.id = 0;
-    // sprint.sprintNumber = 0;
-    // sprint.startDate = '';
-    // sprint.endDate = '';
-    // sprint.state = '';
-    // sprint.team = {} as Team;
-    // sprint.workUnit = ''
-    //   let sprintKeys = Object.keys(sprint);
-    //   for (let object of processedJson.properties) {
-    // let x = object.key;
-    // let splittedKeys = x.split("_");
-    // var actualKey = splittedKeys[splittedKeys.length - 1];
-    //     console.log(actualKey);
-    //     if (sprintKeys.includes(actualKey)) {
+// sprint.sprintNumber=
+// sprint.id = 0;
+// sprint.sprintNumber = 0;
+// sprint.startDate = '';
+// sprint.endDate = '';
+// sprint.state = '';
+// sprint.team = {} as Team;
+// sprint.workUnit = ''
+//   let sprintKeys = Object.keys(sprint);
+//   for (let object of processedJson.properties) {
+// let x = object.key;
+// let splittedKeys = x.split("_");
+// var actualKey = splittedKeys[splittedKeys.length - 1];
+//     console.log(actualKey);
+//     if (sprintKeys.includes(actualKey)) {
 
-    //       (sprint as any)[actualKey] = object.value;
+//       (sprint as any)[actualKey] = object.value;
 
-    //       let index = sprintKeys.indexOf(actualKey);
-    //       sprintKeys.splice(index, 1);
+//       let index = sprintKeys.indexOf(actualKey);
+//       sprintKeys.splice(index, 1);
 
-    //     }
+//     }
 
-    //   }
-    //   console.log(sprint);
-    //   return sprint;
-
-
-
+//   }
+//   console.log(sprint);
+//   return sprint;
