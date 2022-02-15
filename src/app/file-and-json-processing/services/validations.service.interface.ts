745c8388399046f481582dfe440488647b0eeea1
krishnaCG-1997 <krishna.a.agarwal@capgemini.com>
2022-02-15 16:22:18 +0530

    sonar validations done

--- src/app/file-and-json-processing/services/validations.service.interface.ts
+++ src/app/file-and-json-processing/services/validations.service.interface.ts
@@ -2,4 +2,5 @@ import { Group } from '../models/group';
 
 export interface IValidationService {
   validateJira(data: Group[]): Boolean;
+  validateSonar(data: Group[]): Boolean;
 }
--- src/app/file-and-json-processing/services/validations.service.ts
+++ src/app/file-and-json-processing/services/validations.service.ts
@@ -1,16 +1,53 @@
+//import { CodeQualitySnapshot } from '../../data-ingestion/model/entities/code-quality-snapshot.entity';
 import { Group } from '../models/group';
 import { IValidationService } from './validations.service.interface';
 
 export class ValidationService implements IValidationService {
   validateJira(data: Group[]): Boolean {
+    console.log(data);
     return true;
   }
 
-  private isNotNull(value: any): Boolean {
-    if (!value) {
-      return false;
+  validateSonar(data: Group[]): Boolean {
+    for (let group of data) {
+      for (let object of group.properties) {
+        let key = object.key;
+        let splittedKeys = key.split('_');
+        var actualKey = splittedKeys[splittedKeys.length - 1];
+        if (actualKey === 'bugs') {
+          this.isNotNull(object.value);
+          let result = this.isNumber(object.value);
+          this.checkError(result);
+        }
+        if (actualKey === 'codeSmells') {
+          this.isNotNull(object.value);
+          let result = this.isNumber(object.value);
+          this.checkError(result);
+        }
+        if (actualKey === 'codeCoverage') {
+          this.isNotNull(object.value);
+          let result = this.isNumber(object.value);
+          this.checkError(result);
+        }
+        if (actualKey === 'qualityGateStatus') {
+          this.isNotNull(object.value);
+          let result = this.isString(object.value);
+          this.checkError(result);
+        }
+        if (actualKey === 'analysisDate') {
+          this.isNotNull(object.value);
+          let result = this.isString(object.value);
+          this.checkError(result);
+        }
+      }
     }
     return true;
+  }
+
+  private isNotNull(value: any): any {
+    if (!value) {
+      this.checkError(false);
+    }
   }
 
   private isString(value: any): Boolean {
@@ -25,5 +62,11 @@ export class ValidationService implements IValidationService {
       return true;
     }
     return false;
+  }
+
+  private checkError(value: Boolean): any {
+    if (!value) {
+      throw new Error('Exception Occured');
+    }
   }
 }
