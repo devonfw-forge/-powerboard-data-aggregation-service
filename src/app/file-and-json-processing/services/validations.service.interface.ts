98cd41d8e76e9d9b1e175560fefaf8e15b19be16
krishnaCG-1997 <krishna.a.agarwal@capgemini.com>
2022-03-15 18:50:07 +0530

    documentation done for validation service

--- src/app/file-and-json-processing/services/validations.service.interface.ts
+++ src/app/file-and-json-processing/services/validations.service.interface.ts
@@ -3,5 +3,5 @@ import { Group } from '../models/group';
 export interface IValidationService {
   validateJira(data: Group[]): boolean;
   validateSonar(data: Group[]): boolean;
-  validateClientStisfaction(data: Group[]): boolean;
+  validateClientSatisfaction(data: Group[]): boolean;
 }
