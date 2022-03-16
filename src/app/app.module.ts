eec069417e0241a10376856da80b11762868db3c
Hussain <azhar.a.hussain@capgemini.com>
2022-03-16 14:48:57 +0530

    schedular added to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule/dist/schedule.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
@@ -6,7 +7,7 @@ import { DataIngestionModule } from './data-ingestion/data-ingestion.module';
 import { DataProcessingModule } from './data-processing/data-processing.module';
 
 @Module({
-  imports: [CoreModule, DataProcessingModule, DataIngestionModule],
+  imports: [CoreModule, DataProcessingModule, DataIngestionModule, ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
