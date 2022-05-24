c018c0bf532714220f0067390a419803082caebe
LANJEWAR <sakshi-sharad.lanjewar@capgemini.com>
2022-05-24 11:29:14 +0530

    column name refactored

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -3,6 +3,8 @@ export const code_smell = 'codeSmells';
 export const code_coverage = 'codeCoverage';
 export const status = 'qualityGateStatus';
 export const snapshot_time = 'analysisDate';
+
+//jira data below
 export const sprint_number = 'sprintNumber';
 export const start_date = 'startDate';
 export const state = 'state';
@@ -11,6 +13,6 @@ export const work_unit = 'workUnit';
 export const value = 'value ';
 export const committed = 'committed';
 export const completed = 'completed';
-export const jira_snapshot_time = 'snapshotTime';
+export const jira_snapshot_time = 'dateTime';
 export const team_spirit_rating = 'teamSpiritRating';
 export const client_rating = 'clientRating';
